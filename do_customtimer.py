f42938e7588d4e468e153bf892298c371edb91ca
eeganr <whiz.eram@gmail.com>
2022-05-16 16:11:02 -0700

    linting

--- do_customtimer.py
+++ do_customtimer.py
@@ -1,10 +1,9 @@
-from interface.counter import *
+from interface.counter import counter_do
 
-data = [2,1,2,1,2,1]
+data = [2, 1, 2, 1, 2, 1]
 
-samps=len(data)
+samps = len(data)
 
-#Must add extra delay to the start as that is considered the start before any data generation.
-delays=[0.1, 0.2, 0.3, 0.2, 0.1, 0.2]
+delays = [0.1, 0.2, 0.3, 0.2, 0.1, 0.2]
 
 counter_do(data, delays)
--- interface/counter.py
+++ interface/counter.py
@@ -4,25 +4,27 @@ from nidaqmx.constants import AcquisitionType
 import nidaqmx.stream_writers
 from nidaqmx.errors import DaqError
 
-def counter_do(data, delays, initial_delay=0.001, ctr=0,port=0,line='0:7', rate=1000000):
+
+def counter_do(data, delays, initial_delay=0.001, ctr=0, port=0, line='0:7', rate=1000000):
     
     # prechecks to ensure lengths are okay
     if len(data) != len(delays):
         raise Exception('data length does not match lengths provided!')
     
-    # modifying delays
-    
-    data=np.array(data,dtype=np.uint8)
+    # turning data into numpy array
+    data = np.array(data, dtype=np.uint8)
+
+    # modifying delay format
     
     low, high = uniform_to_lowhigh(delays)
     low = [initial_delay] + low
     low = low[:-1]
 
-    testlow=np.array(low)
-    testhigh=np.array(high)
+    testlow = np.array(low)
+    testhigh = np.array(high)
     
     # creating tasks for counter and output channels
-    counter=nidaqmx.Task()
+    counter = nidaqmx.Task()
     counter.co_channels.add_co_pulse_chan_time("Dev1/ctr"+str(ctr))
     
     output = nidaqmx.Task()
@@ -35,32 +37,32 @@ def counter_do(data, delays, initial_delay=0.001, ctr=0,port=0,line='0:7', rate=
     )
     
     output.timing.cfg_samp_clk_timing(
-        rate = rate,
-        source = 'Ctr0InternalOutput',
+        rate=rate,
+        source='Ctr0InternalOutput',
         sample_mode=nidaqmx.constants.AcquisitionType.FINITE,
         samps_per_chan=len(low)
     )
 
     # output generation attempt
     try:
-        sw=nidaqmx.stream_writers.CounterWriter(counter.out_stream)
-        sw.write_many_sample_pulse_time(testhigh,testlow)
+        sw = nidaqmx.stream_writers.CounterWriter(counter.out_stream)
+        sw.write_many_sample_pulse_time(testhigh, testlow)
         
         streamwriter = nidaqmx.stream_writers.DigitalSingleChannelWriter(output.out_stream)
         streamwriter.write_many_sample_port_byte(data)
-        
+
         counter.start()
         output.start()
-          
+
         output.wait_until_done(timeout=sum(low)+sum(high))
         counter.wait_until_done(timeout=sum(low)+sum(high))
-        
+
         output.stop()
         output.close()
-        
+
         counter.stop()
         counter.close()
-        
+
     except DaqError as e:
         raise RuntimeError(f'An error occured while running: {e}')
         output.stop()
@@ -68,15 +70,17 @@ def counter_do(data, delays, initial_delay=0.001, ctr=0,port=0,line='0:7', rate=
         
         counter.stop()
         counter.close()
-        
+
+     
 def uniform_to_lowhigh(arr):
-    lows=[]
-    highs=[]
+    lows = []
+    highs = []
     for val in arr:
         lows.append(val/2)
         highs.append(val/2)
     return lows, highs
 
+
 def delay_to_lowhigh(delay, samps):
-    vals=[delay/2]*samps
+    vals = [delay/2]*samps
     return vals, vals
