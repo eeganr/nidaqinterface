5cc42d5afa5a2060836914ba5e1f1455b270a75b
Eegan Ram <67843748+eeganr@users.noreply.github.com>
2022-05-16 15:40:55 -0700

    customtimer simplification

--- do_customtimer.py
+++ do_customtimer.py
@@ -1,20 +1,10 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Apr 14 16:00:23 2022
-
-@author: User
-"""
-
 from interface.counter import *
 
-data = [1,2,1,2,1,2,1,2,1,2,1]
+data = [2,1,2,1,2,1]
 
 samps=len(data)
 
-delays=[0.2,0.5,0.1,0.7,0.1,0.2,0.2,0.3,0.1,0.4]
+#Must add extra delay to the start as that is considered the start before any data generation.
+delays=[0.1, 0.2, 0.3, 0.2, 0.1, 0.2]
 
-lows,highs = uniform_to_lowhigh(delays)
-
-data=np.array(data,dtype=np.uint8)
-
-counter_do(data,lows,highs)
+counter_do(data, delays)
