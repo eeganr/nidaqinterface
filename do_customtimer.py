548ca7d09df31467c2d1625be07780291595e8db
Eegan Ram <67843748+eeganr@users.noreply.github.com>
2022-05-24 14:49:05 -0700

    docustomtimer run from csv

--- do_customtimer.py
+++ do_customtimer.py
@@ -4,6 +4,8 @@ c = Counter({'camera':2,'aom':0,'count':1,'yeetmobile':3,'gate':4})
 
 data = [2, 1, 2, 1, 2, 1]
 
-instructions = ['camera+aom',4,'count+yeetmobile',5,'gate',2]
+instructions = ['camera+aom',1,'count+yeetmobile',2,'gate',1]
 
-c.run(instructions)
+#c.run(instructions)
+
+c.run_from_csv('exampleinstructions.csv')
